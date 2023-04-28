7aca54c9d62597659adbd690da009f4a87ddb542
Andres B <19674617+http-andres2122@users.noreply.github.com>
2023-04-28 07:30:50 -0500

    se implementan mejoras para cliente

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 import { valida } from "./validaciones.js";
 
-const inputs = document.querySelectorAll("input", "textarea");
+const inputs = document.querySelectorAll("input");
+const textarea = document.querySelectorAll("textarea");
 
 inputs.forEach((input) => {
     input.addEventListener("blur", (input) => {
         valida(input.target);
-    });
+    })
+});
+
+textarea.forEach((input) => {
+    input.addEventListener("blur", (input) => {
+        valida(input.target);
+    })
 });
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,17 +1,56 @@
 export function valida(input) {
-    const tipoDeInput = input.dataset.tipo;
+    const tipoDeInput = input.dataset.contacto;
     if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input);
-    };
-    console.log(input);
-
-    if(input.validity.valid){
-        console.log("y")
+        // console.log(tipoDeInput);
     }
+    // console.log(tipoDeInput);
+console.log(input.parentElement);
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+];
+
+const mensajeDeError = {
+    nombre: {
+        valueMissing: "verifica el nombre",
+    },
+    correo: {
+        valueMissing: "verica el correo",
+        typeMismatch: "El correo no es valido",
+    },
+    asunto: {
+        valueMissing: "verifica el asunto",
+    },
+    msj: {
+        valueMissing: "verifica el mensaje",
+    },
 };
 
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach( (error) => {
+        if(input.validity[error]) {
+        console.log(error);
+        console.log(input.validity[error]);
+        console.log(mensajeDeError[tipoDeInput][error]);
+        mensaje = mensajeDeError[tipoDeInput][error];
+        }
+    });
+    return mensaje
+}
+
+
 const validadores = {
-    nombre: console.log(2),
-    correo: console.log (3)
+    
 }
 
